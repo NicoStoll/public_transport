2a09db210ec47f041afabf4cc0d01eb41bc6ba68
nicolas <41stni1mst@hft-stuttgart.de>
2025-03-26 14:11:49 +0100

    Project Setup

---
+++ src/test/java/de/stoll/nicolas/transport/TransportApplicationTests.java
@@ -0,0 +1,13 @@
+package de.stoll.nicolas.transport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
